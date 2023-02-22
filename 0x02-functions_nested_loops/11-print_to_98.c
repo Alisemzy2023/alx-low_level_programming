1df00d78f7aedcee6aaa5549d16007b4d5f3beef
alisemzy2023 <aliabdulhafeezsemomhe@gmail.com>
2023-02-22 08:57:17 -0800

    corrected 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,13 @@
 #include "main.h"
 #include "6-abs.c"
 #include <stdio.h>
-
+/**
+ * print_to_98 - prints numbers up to 98
+ * 
+ * 
+ *
+ * Return: nothing
+ */
 void print_to_98(int n)
 {
 	int i;
